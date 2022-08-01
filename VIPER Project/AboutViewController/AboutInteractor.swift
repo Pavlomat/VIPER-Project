36a0735fe8d6350f3e89f05c51c9b8750bc5eb46
Matvey Pavlov <75501963+Pavlomat@users.noreply.github.com>
2022-08-01 09:54:33 +0300

    Update AboutInteractor.swift

--- VIPER Project/AboutViewController/AboutInteractor.swift
+++ VIPER Project/AboutViewController/AboutInteractor.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-protocol AboutInteractorProtocol: AnyObject {   //логика хранения и извлечения данных
+protocol AboutInteractorProtocol: AnyObject {
 }
 
 class AboutInteractor: AboutInteractorProtocol {
     
-    weak var presenter: AboutPresenterProtocol! //Обязательно WEAK чобы не было утечек (VC - Strong и далее Presenter, Interactor и тд уже weak) Во всей цепочке главный - VC
+    weak var presenter: AboutPresenterProtocol!
     
     required init(presenter: AboutPresenterProtocol) {
         self.presenter = presenter
